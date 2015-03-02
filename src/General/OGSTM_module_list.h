759a70515d82a54ae29e23b1113930b5c532ef9e
plazzari <plazzari@99e410ec-aefa-4077-b8ee-b4c84c4c781a>
2015-03-02 09:25:52 +0000

    :- Fmpplib             --> cleaned mppstop    OGSTM_module_list.h --> created list of all modules    ogstm               --> organized the model run in ogstm_initialize, step and                           ogstm_finalize

---
+++ src/General/OGSTM_module_list.h
@@ -0,0 +1,17 @@
+       USE myalloc
+       USE myalloc_mpp
+       USE IO_mem
+       USE FN_mem
+       USE ADV_mem
+       USE HDF_mem
+       USE ZDF_mem
+       USE OPT_mem
+       USE BC_mem
+       USE BIO_mem
+       USE DIA_mem
+       USE CALENDAR
+       USE time_manager
+#ifdef Mem_Monitor
+       USE check_mem
+       USE iso_c_binding
+#endif Mem_Monitor

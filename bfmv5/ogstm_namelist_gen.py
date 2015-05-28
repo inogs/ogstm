ddb462e49f6b44fbc59c9aca08ce518f97841836
Giorgio Bolzon <gbolzon@ogs.trieste.it>
2015-05-28 10:29:36 +0200

    ACHIEVING 2D DIAGNOSTICS
    in the ogstm/src:
    - trcbio now calls BFM0D_Output_EcologyDynamics_surf(b, c, d ,d2)
       d2(jptra_dia_2d) is the array of 2d diagnostics
    -  modulo.F does not define jptra_dia_2d
    
    in the bfmv5 part:
    - updating namelist.passivetrc example file (only 3D and 2D variables)
    - adding 2D variable list in BFMtab.xml (and making ogstm_namelist_gen.py able to read it)

--- bfmv5/ogstm_namelist_gen.py
+++ bfmv5/ogstm_namelist_gen.py
@@ -11,8 +11,14 @@ def file2stringlist(filename):
 xmldoc = minidom.parse('BFMtab.xml')
 
 ORIG_NAMELIST=file2stringlist("namelist.passivetrc")
+endnamelist1 = ORIG_NAMELIST.index("/")
+endnamelist2 = ORIG_NAMELIST.index("/",endnamelist1+1)
+endnamelist3 = ORIG_NAMELIST.index("/",endnamelist2+1)
+
+
 XML_MODELVARS={}
 XML_DIA__VARS={}
+XML_DIA2dVARS={}
 
 NODES=xmldoc.getElementsByTagName("ModelVars")[0].getElementsByTagName("var")
 for node in NODES:
@@ -35,6 +41,18 @@ for node in NODES:
         print var + " is already defined."
         sys.exit(1)        
 
+NODES=xmldoc.getElementsByTagName("Diagnostics_2D")[0].getElementsByTagName("var")
+for node in NODES:
+    var = str(node.getAttribute("name"))
+    high= node.getAttribute("highfreq")=='true'
+    if not XML_DIA2dVARS.has_key(var):
+        XML_DIA2dVARS[var] = high
+    else:
+        print var + " is already defined."
+        sys.exit(1) 
+
+
+
 MODEL_HF=[]
 MODEL_LF=[]
 DIA_HF  =[]
@@ -43,7 +61,8 @@ DIA_LF  =[]
 NAMELIST_NEW=[]
 NAMELIST_NEW.append("&NATTRC \n")
 
-for il, line in enumerate(ORIG_NAMELIST):
+SECTION_NAMELIST=ORIG_NAMELIST[:endnamelist1]
+for il, line in enumerate(SECTION_NAMELIST):
     if line.find("ctrcnm") != -1:
         quote_1=line.find("\"")
         quote_2=line.find("\"",quote_1+1)
@@ -54,7 +73,7 @@ for il, line in enumerate(ORIG_NAMELIST):
             ind   = int(line[par_1+1:par_2])
             
             NAMELIST_NEW.append(line +"\n")
-            NAMELIST_NEW.append(ORIG_NAMELIST[il+1]+"\n")
+            NAMELIST_NEW.append(SECTION_NAMELIST[il+1]+"\n")
             NAMELIST_NEW.append("   ctrmax(%d)=%e \n" % (ind, XML_MODELVARS[varname][0]))
             NAMELIST_NEW.append("   ctr_hf(%d)=%d \n" % (ind, XML_MODELVARS[varname][1]))
             NAMELIST_NEW.append("\n")
@@ -69,9 +88,9 @@ for il, line in enumerate(ORIG_NAMELIST):
             
 NAMELIST_NEW.append("/ \n\n")
 
-
+SECTION_NAMELIST=ORIG_NAMELIST[endnamelist1:endnamelist2]
 NAMELIST_NEW.append("&NATTRC_DIAG\n")
-for il, line in enumerate(ORIG_NAMELIST):
+for il, line in enumerate(SECTION_NAMELIST):
     if line.find("dianm") != -1:
         quote_1=line.find("\"")
         quote_2=line.find("\"",quote_1+1)
@@ -82,7 +101,7 @@ for il, line in enumerate(ORIG_NAMELIST):
             ind   = int(line[par_1+1:par_2])
             
             NAMELIST_NEW.append(line +"\n")
-            NAMELIST_NEW.append(ORIG_NAMELIST[il+1]+"\n")
+            NAMELIST_NEW.append(SECTION_NAMELIST[il+1]+"\n")
             NAMELIST_NEW.append("   diahf(%d)=%d \n" % (ind, XML_DIA__VARS[varname]))
             NAMELIST_NEW.append("\n")
             
@@ -95,6 +114,33 @@ for il, line in enumerate(ORIG_NAMELIST):
             sys.exit(1)
 NAMELIST_NEW.append("/ \n\n")
 
+SECTION_NAMELIST=ORIG_NAMELIST[endnamelist2:]
+NAMELIST_NEW.append("&NATTRC_DIAG_2D\n")
+for il, line in enumerate(SECTION_NAMELIST):    
+    if line.find("dianm") != -1:
+        quote_1=line.find("\"")
+        quote_2=line.find("\"",quote_1+1)
+        varname=line[quote_1+1:quote_2]
+        if XML_DIA2dVARS.has_key(varname):
+            par_1 = line.find("(")
+            par_2 = line.find(")")
+            ind   = int(line[par_1+1:par_2])
+            
+            NAMELIST_NEW.append(line.replace("dianm","dianm_2d")+"\n" )
+            NAMELIST_NEW.append(SECTION_NAMELIST[il+1].replace("diaun","diaun_2d") +"\n")
+            NAMELIST_NEW.append("   diahf_2d(%d)=%d \n" % (ind, XML_DIA2dVARS[varname]))
+            NAMELIST_NEW.append("\n")
+            
+            if XML_DIA2dVARS[varname]:
+                DIA_HF.append(varname)
+            else:
+                DIA_LF.append(varname)
+        else:
+            print "Error: " + varname + " not defined !"
+            sys.exit(1)
+NAMELIST_NEW.append("/ \n\n")
+
+
 
       
 
@@ -102,10 +148,11 @@ F=open("namelist.passivetrc_new","w")
 F.writelines(NAMELIST_NEW)
 F.close()
 
-
-print "HF variables : " , len(MODEL_HF), "Model", len(DIA_HF), "Diagnostics"
-print "LF variables : " , len(MODEL_LF), "Model", len(DIA_LF), "Diagnostics"
-
-print "HF LIST:"
-for var in MODEL_HF: print var
-for var in DIA_HF : print var
+printout=True
+if printout:
+    print "HF variables : " , len(MODEL_HF), "Model", len(DIA_HF), "Diagnostics"
+    print "LF variables : " , len(MODEL_LF), "Model", len(DIA_LF), "Diagnostics"
+    
+    print "HF LIST:"
+    for var in MODEL_HF: print var
+    for var in DIA_HF : print var

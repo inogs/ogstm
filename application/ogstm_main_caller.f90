bb7d4189dde93313149bd1ff321b421e00f12583
Eric Pascolo <eric.pascolo@gmail.com>
2016-10-21 15:49:06 +0200

    delete old main

--- application/ogstm_main_caller.f90
+++ application/ogstm_main_caller.f90
@@ -1,20 +1,21 @@
 PROGRAM OGSTM_MAIN
 !
+!
 !                       PROGRAM MAIN
 !                     ******************
 !
-!
+
       USE ogstm
       implicit none
       integer :: info,ierr
       write(*,*) "Starting ..."
       CALL mpi_init(ierr)
 
-!$OMP PARALLEL
-!$OMP MASTER
+      !$OMP PARALLEL
+      !$OMP MASTER
       CALL ogstm_launcher()
-!$OMP END MASTER
-!$OMP END PARALLEL
+      !$OMP END MASTER
+      !$OMP END PARALLEL
 
       CALL mpi_finalize(info)
       

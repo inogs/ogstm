fbf883f83d935627d5ecd372c5e6d8d038f004fe
Pierluigi Di Cerbo <pierluigi.dicerbo@gmail.com>
2017-04-21 17:57:29 +0200

    trcdifDA.f90 "conversion" completed :)

--- src/DA/trcditDA.f90
+++ src/DA/trcditDA.f90
@@ -1,4 +1,4 @@
-      SUBROUTINE trcdit(datemean,datefrom,dateTo,FREQ_GROUP)
+      SUBROUTINE trcditDA(datemean,datefrom,dateTo)
 !---------------------------------------------------------------------
 !
 !                       ROUTINE trcdit
@@ -25,18 +25,17 @@
 ! local declarations
 ! ==================
       CHARACTER(LEN=17), INTENT(IN) :: datemean, dateFrom, dateTo
-      INTEGER, INTENT(IN) :: FREQ_GROUP ! 1 = HIGH FREQ, 2 = LOW FREQ
       double precision ::  Miss_val =1.e20
 
       INTEGER jk,jj,ji, jn, jn_high,i,j,k
-      INTEGER ind, i_contribution, j_contribution
-      INTEGER ave_counter
+      INTEGER ind, jn2, i_contribution, j_contribution
+      INTEGER s, nc, counter
 
-      CHARACTER(LEN=56) output_file_nc  ! AVE_FREQ_1/ave.20091231-12:00:00.P1n.nc
-      CHARACTER(LEN=20)  var
-      CHARACTER(LEN=60) bkpname
-      CHARACTER(LEN=11) DIR
-      logical IsBackup
+      CHARACTER(LEN=39) output_file_nc  ! DA/ave.20091231-12:00:00.P1n.nc
+      CHARACTER(LEN=3)  var
+!!      CHARACTER(LEN=60) bkpname
+!!      CHARACTER(LEN=11) DIR
+!!      logical IsBackup
 
 !----------------------------------------------------------------------
 ! statement functions
@@ -49,37 +48,32 @@
       INTEGER totjstart, totjend, reljstart, reljend
 
 ! ----------------------------------------
-       IsBackup =  (datemean.eq.dateTo)
-       if (lwp) write(*,*) 'trcdit IsBackup = ',IsBackup, ' group ' ,FREQ_GROUP
-! ----------------------------------------
-       bkpname  = 'AVE_FREQ_1/ave.20111231-15:30:00.N1p.nc.bkp'
       call mppsync()
 
-      jn_high = 0
+      if (lwp)  CHLtot = 0.0
 
-      SELECT CASE (FREQ_GROUP)
-        CASE (1) 
-       ave_counter=ave_counter_1 
-       DIR='AVE_FREQ_1/'
-        CASE (2) 
-       ave_counter=ave_counter_2 
-       DIR='AVE_FREQ_2/'
-      END SELECT
-      if (lwp) tottrnIO = Miss_val
 !  Ghost Shells - Manual s Indexes-
 
-      DO jn=1,jptra ! DO LABEL 5
+      DO jn=1,17 ! DO LABEL 5
+        var = varlistDA(jn)
 
-      if (.not.is_time_to_save(jn,FREQ_GROUP)) CYCLE
-      if (FREQ_GROUP.eq.1) jn_high = jn_high+1
+        jn2 = GET_INDEX(var)
+        jn_high = GET_HIGHFREQ(jn2)
 
-      var    = ctrcnm(jn)
-      output_file_nc = DIR//'ave.'//datemean//'.'//trim(var)//'.nc'
-      bkpname        = DIR//'ave.'//datemean//'.'//trim(var)//'.nc.bkp'
+        output_file_nc = 'DA__FREQ_1/ave.'//datemean//'.'//var//'.nc'
+
+!         In order to avoid zeros in output_file_nc, when we reduce to float32
+          do ji=1,jpi
+          do jj=1,jpj
+          do jk=1,jpk
+           if (traIO_HIGH(jk,jj,ji,jn_high).lt.SMALL)  traIO_HIGH(jk,jj,ji,jn_high)=SMALL
+          enddo
+          enddo
+          enddo
 
 ! *************** START COLLECTING DATA *****************************
       if(myrank == 0) then                    ! IF LABEL 1
-
+          write(*,*) var, ' jn =',jn,jn2,jn_high
 ! ******* myrank 0 sets indexes of tot matrix where to place its own part
           iPd    = nldi
           iPe    = nlei
@@ -100,11 +94,8 @@
 
 
 !***** START ASSEMBLING ***  myrank 0 puts its tracer part in the tot matrix
-          if (FREQ_GROUP.eq.1) then
+
           tottrnIO(:,totjstart:totjend,totistart:totiend)= traIO_HIGH(:,reljstart:reljend,relistart:reliend,jn_high)
-          else
-          tottrnIO(:,totjstart:totjend,totistart:totiend)= traIO (:,reljstart:reljend,relistart:reliend,jn)
-          endif
 
 
            do idrank = 1,mpi_glcomm_size-1
@@ -140,31 +131,11 @@
               enddo
             enddo
           enddo
-!      ciclo vecchia maniera
-!          do jk =1 , jpk
-!           do jj =totjstart,totjend
-!             do ji =totistart,totiend
-!                 ind = (ji-totistart+ relistart)+ (jj-totjstart+ reljstart-1)*jpi_rec+(jk-1)*jpj_rec*jpi_rec
-!                 tottrnIO(jk,jj,ji)= bufftrn   (ind)
-!             enddo
-!            enddo
-!           enddo
        enddo !idrank = 1, size-1
    else  ! IF LABEL 1,  if(myrank == 0)
 ! **** work of the other ranks
 ! ****** 1. load  inf buffer their IO matrices
-     if (FREQ_GROUP.eq.2) then
-        do ji =1 , jpi
-          i_contribution= jpk*jpj * (ji - 1 )
-         do jj =1 , jpj
-          j_contribution=jpk*(jj-1)
-          do jk =1 , jpk
-            ind            =  jk + j_contribution + i_contribution
-            bufftrn   (ind)= traIO( jk,jj,ji,jn)
-          enddo
-         enddo
-       enddo
-     else ! FREQ_GROUP.eq.1
+
         do ji =1 , jpi
           i_contribution= jpk*jpj * (ji - 1 )
          do jj =1 , jpj
@@ -175,7 +146,7 @@
           enddo
          enddo
        enddo
-     endif
+
 ! ******  2.send buffer to myrank 0
        call MPI_SEND(jpi  , 1,mpi_integer, 0, 1, mpi_comm_world,ierr)
        call MPI_SEND(jpj  , 1,mpi_integer, 0, 2, mpi_comm_world,ierr)
@@ -195,56 +166,65 @@
 ! *********** START WRITING **************************
       if(myrank == 0) then ! IF LABEL 4
 
-
-        if (IsBackup) then
-          CALL WRITE_AVE_BKP(bkpname,var,datefrom, dateTo,tottrnIO,ave_counter)
-
-        else
-
           CALL WRITE_AVE(output_file_nc,var,datefrom, dateTo, tottrnIO)
-      
-         endif
+          if (isaCHLVAR(VAR)) then
+            do ji=1,jpiglo
+              do jj=1,jpjglo
+                do jk=1,jpk
+                  CHLtot(ji,jj,jk) = CHLtot(ji,jj,jk) + REAL(tottrnIO(jk,jj,ji),4) ! d2f3d ! remains in RAM, used by snutell
+                end do
+              end do
+            end do
 
+          endif
 
       end if ! IF LABEL 4  if(myrank == 0)
 
-! 200     FORMAT(' ',A3,I4,I4,I4,I4,D30.23)
 
-
-
-      if (.not.IsBackup) then
-         if (FREQ_GROUP.eq.2) then
-            traIO(:,:,:,jn) = 0.  !      we reset matrix for new average
-         else
-            traIO_HIGH(:,:,:,jn_high) = 0.
-         endif
-      endif
-
-      END DO ! do jn=1,jptra , DO LABEL 5
+      END DO ! do jn=1,17 , DO LABEL 5
 
       ! stop
 
 
 
 
-      if(myrank == 0) WRITE(numout,*) '**** trcdit : write NetCDF passive tracer concentration'
+      if(myrank == 0) then ! WRITE(numout,*) '**** trcdit : write NetCDF passive tracer concentration'
+
+        s = nf90_create(CHLSUP_FOR_DA, NF90_CLOBBER, nc)
+        s= nf90_def_dim(nc,'lon'           , jpiglo,  xid)
+        s= nf90_def_dim(nc,'lat'           , jpjglo,  yid)
+        s= nf90_def_dim(nc,'depth'         , 1   ,depid)
+        s= nf90_def_dim(nc,'time'  , NF90_UNLIMITED,timid)
+        s = nf90_def_var(nc, 'lchlm', nf90_float, (/xid,yid,depid,timid/),  idVAR)
+
+        s = nf90_put_att(nc,idVAR, 'missing_value',4.e+20)
+
+        s =nf90_enddef(nc)
+
+        CHL_SUP = CHLtot(:,:,1)
+        s = nf90_put_var(nc, idVAR  ,CHL_SUP ); call handle_err1(s,counter,CHLSUP_FOR_DA)
+        s=nf90_close(nc)
+
+      endif
 
       CONTAINS
 
-      LOGICAL FUNCTION IS_TIME_TO_SAVE(jn,FREQ_GROUP)
-      IMPLICIT NONE
+      INTEGER FUNCTION GET_INDEX(VARNAME)
+      CHARACTER(LEN=3), INTENT(IN) :: VARNAME
+      INTEGER i
+      GET_INDEX=0
+      do i =1, jptra
+         if (ctrcnm(i).eq.VARNAME) GET_INDEX = i
+      enddo
+      END FUNCTION GET_INDEX
 
-      integer jn, FREQ_GROUP
+      INTEGER FUNCTION GET_HIGHFREQ(varind)
+      integer, INTENT(IN) :: varind
+      integer i
+      GET_HIGHFREQ = 0
+      do i=1,jptra_high
+         if (highfreq_table(i).eq.varind) GET_HIGHFREQ = i
+      enddo
+      END FUNCTION GET_HIGHFREQ
 
-      IF (FREQ_GROUP.eq.2) then
-         IS_TIME_TO_SAVE = .true.
-      ELSE
-        IF (ctr_hf(jn).eq.1) then
-            IS_TIME_TO_SAVE = .true.
-        ELSE
-           IS_TIME_TO_SAVE = .false.
-         ENDIF
-      ENDIF
-      END FUNCTION IS_TIME_TO_SAVE
-
-      END SUBROUTINE trcdit
+      END SUBROUTINE trcditDA

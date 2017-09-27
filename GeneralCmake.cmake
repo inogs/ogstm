354508e94d6728af658959fcdc1c6e423c692739
Giorgio Bolzon <gbolzon@ogs.trieste.it>
2017-09-27 17:38:15 +0200

    Adding -extend_source as debug flag

--- GeneralCmake.cmake
+++ GeneralCmake.cmake
@@ -32,7 +32,7 @@ add_definitions(-Dkey_mpp -Dkey_mpp_mpi)
 if (MPI_Fortran_COMPILER MATCHES "mpiifort.*")
   # mpiifort
   set (CMAKE_Fortran_FLAGS_RELEASE "-fno-math-errno -O2 -xAVX -qopt-report5 -g -cpp -align array64byte") #-qopenmp
-  set (CMAKE_Fortran_FLAGS_DEBUG   "-O0 -g -cpp -CB -fp-stack-check -check all -traceback -gen-interfaces -warn interfaces -fpe0") #-qopenmp
+  set (CMAKE_Fortran_FLAGS_DEBUG   "-O0 -g -cpp -CB -fp-stack-check -check all -traceback -gen-interfaces -warn interfaces -fpe0 -extend_source") #-qopenmp
 elseif (MPI_Fortran_COMPILER MATCHES "mpif90.*")
   # mpif90
   set (CMAKE_Fortran_FLAGS_RELEASE "-O2  -fimplicit-none -cpp  -ffixed-line-length-132")

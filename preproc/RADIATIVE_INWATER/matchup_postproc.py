b617d74651ce829c3b3bf8a3449ad26296821b5a
Elena <eterzic@inogs.it>
2019-10-19 19:02:42 +0200

    Code update

--- preproc/RADIATIVE_INWATER/matchup_postproc.py
+++ preproc/RADIATIVE_INWATER/matchup_postproc.py
@@ -47,9 +47,9 @@ for i, path in enumerate(pathlist):
     fig,ax = plt.subplots(1,3, gridspec_kw = {'wspace':0.25, 'hspace':0.5})
     fig.set_size_inches(15,5)
     
-    ax1 = plot_matchup('lin', L380, ax, 'indigo', 0, 'Ed 380', 0.70, 0.30,False, None)
-    ax2 = plot_matchup('lin', L412, ax, 'darkcyan', 1, 'Ed 412', 0.70, 0.30, False, None)
-    ax3 = plot_matchup('lin', L490, ax, 'navy', 2, 'Ed 490', 0.70, 0.30, False, None)
+    ax1 = plot_matchup('lin', L380, ax, 'indigo'  , 0, '$Ed _{\lambda=380}$', 0.60, 0.40, False, None)
+    ax2 = plot_matchup('lin', L412, ax, 'darkcyan', 1, '$Ed _{\lambda=412}$', 0.60, 0.40, False, None)
+    ax3 = plot_matchup('lin', L490, ax, 'navy'    , 2, '$Ed _{\lambda=490}$', 0.60, 0.40, False, None)
     
     outdir = '/galileo/home/userexternal/eterzic0/CODE/ogstm/preproc/RADIATIVE_INWATER/PLOTS/'
     fig.savefig(outdir + 'Scatter_' + plotname[i] + '.png', format='png', dpi=400)

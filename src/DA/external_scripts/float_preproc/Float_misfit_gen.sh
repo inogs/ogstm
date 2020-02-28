3019191caa1bfd5b21a21f3350df78c3e1631dd3
Giorgio Bolzon <gbolzon@inogs.it>
2020-02-28 15:43:48 +0100

    Operational version, a list of variables is supposed to come from caller (job or B2)

--- src/DA/external_scripts/float_preproc/Float_misfit_gen.sh
+++ src/DA/external_scripts/float_preproc/Float_misfit_gen.sh
@@ -7,6 +7,17 @@ echo "Float_misfit_gen.sh [ -t date] "
 echo "EXAMPLE"
 echo 'Float_misfit_gen.sh -t 20150106 '
 echo ""
+echo "These variables exported by caller:"
+echo "ONLINE_REPO"
+echo "MASKFILE"
+echo "BINDIR"
+echo "DA_DIR"
+echo "DA__FREQ_1"
+echo "OPA_VENV_1"
+echo "OPA_BITSEA"
+echo "OPA_HOSTDIR"
+echo "OPA_POSTPROCDIR"
+echo ""
 }
 
 
@@ -23,53 +34,28 @@ for I in 1; do
    shift 2
 done
 
-
-module purge
-module load profile/base
-module load autoload
-module load netcdf/4.6.1--intel--pe-xe-2018--binary
-module load numpy/1.15.2--python--2.7.12 
-module load intelmpi/2018--binary
-
-source /gpfs/work/OGS20_PRACE_P/COPERNICUS/py_env_2.7.12/bin/activate
-export PYTHONPATH=$PYTHONPATH:/gpfs/work/OGS20_PRACE_P/COPERNICUS/bit.sea
-
-export OPA_HOME=/gpfs/scratch/userexternal/ateruzzi/MULTIVARIATE_24/TEST_04/
-export ONLINE_REPO=/gpfs/scratch/userexternal/gbolzon0/CHAIN_V5C/ONLINE_V5C
-export MASKFILE=$OPA_HOME/wrkdir/MODEL/meshmask.nc
+source ${BINDIR}/profile.inc
+opa_prex "module unload numpy"
+opa_prex "source $OPA_VENV_1/bin/activate"
+export PYTHONPATH=${PYTHONPATH}:$OPA_BITSEA
 
 
-     CODEDIR=$OPA_HOME/wrkdir/float_preproc/
-      DA_DIR=$OPA_HOME/wrkdir/MODEL/DA__FREQ_1/
-     BASEDIR=$DA_DIR/PROFILATORE
-    DEST_DIR=$DA_DIR
+BASEDIR=$DA_DIR/PROFILATORE
 
-
-##  V6C version
-#. @@(I:OPA_HOME)/bin/opa_profile.inc
-# opa_prex "module unload numpy"
-# opa_prex "source $OPA_VENV_1/bin/activate"
-# PYTHONPATH=${PYTHONPATH}:$OPA_BITSEA
-# DA_DIR=$OPA_WRKDIR/MODEL/DA__FREQ_1/
-# BASEDIR=$DA_DIR/PROFILATORE
-# export MASKFILE=$OPA_WRKDIR/MODEL/meshmask.nc
-# export ONLINE_REPO=$I_OPA_HOME/inpdir/ONLINE
-
-
-mkdir -p $DA_DIR/links
-mv $DA_DIR/RSTbefore.*0000* $DA_DIR/links
+mkdir -p ${DA__FREQ_1}/links
+mv ${DA__FREQ_1}/RSTbefore.*0000* ${DA__FREQ_1}/links
 
 
 for vv in N3n P_l; do
 
 	rm -rf $BASEDIR
-	mkdir -p $BASEDIR
+	mkdir  $BASEDIR
 
-    VAR_DESCRIPTOR="$OPA_HOME/wrkdir/float_preproc/VarDescriptor_${vv}.xml" # esterni
-    MISFIT_FILE="${DA_DIR}/${DATE}.${vv}_arg_mis.dat"
+    VAR_DESCRIPTOR="${DA_DIR}/VarDescriptor_${vv}.xml"
+       MISFIT_FILE="${DA_DIR}/${DATE}.${vv}_arg_mis.dat"
 
 	if [ $vv == P_l ]; then
-	    python ${CODEDIR}/var_aggregator.py -l RSTbefore.${DATE}*13:00*P1l.nc -i $DA_DIR -d $VAR_DESCRIPTOR -t $DA_DIR -m $MASKFILE
+	    opa_prex_or_die "python ${OPA_POSTPROCDIR}/var_aggregator.py -l RSTbefore.${DATE}*13:00*P1l.nc -i ${DA__FREQ_1} -d $VAR_DESCRIPTOR -t ${DA__FREQ_1} -m $MASKFILE "
 	    DADEP=200
 	fi
 
@@ -78,16 +64,16 @@ for vv in N3n P_l; do
 	fi
 
 
-	python ${CODEDIR}/float_extractor.py -t ${DATE}  -i $DA_DIR -b $BASEDIR -d $VAR_DESCRIPTOR -v $vv
-	python ${CODEDIR}/preproc.py  -t ${DATE}  -i $DA_DIR -b $BASEDIR -m $MASKFILE -v $vv -d $DADEP --misfit $MISFIT_FILE -o $OUTDIR
+	opa_prex_or_die "python ${OPA_HOSTDIR}/float_extractor.py -t ${DATE}  -i ${DA__FREQ_1} -b $BASEDIR -d $VAR_DESCRIPTOR -v $vv "
+	opa_prex_or_die "python ${OPA_HOSTDIR}/preproc.py  -t ${DATE}  -i ${DA__FREQ_1} -b $BASEDIR -m $MASKFILE -v $vv -d $DADEP --misfit $MISFIT_FILE -o ${DA_DIR} "
 
 done
 
-python ${CODEDIR}/merge_arg_mis.py -n ${DA_DIR}/${DATE}.N3n_arg_mis.dat -c ${DA_DIR}/${DATE}.P_l_arg_mis.dat -o ${DA_DIR}/${DATE}.arg_mis.dat
+opa_prex_or_die "python ${OPA_HOSTDIR}/merge_arg_mis.py -n ${DA_DIR}/${DATE}.N3n_arg_mis.dat -c ${DA_DIR}/${DATE}.P_l_arg_mis.dat -o ${DA__FREQ_1}/${DATE}.arg_mis.dat "
 
 
-mv $DA_DIR/links/RSTbefore* $DA_DIR
+mv ${DA__FREQ_1}/links/RSTbefore* ${DA__FREQ_1}
 
-
-
-
+echo "#############################################"
+echo "########### Float_misfit_gen.sh finished ####"
+echo "#############################################"

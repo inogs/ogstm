4667f2f579d4833fc3b9d86c01b010a9c56b4a30
Elena <eterzic@inogs.it>
2020-07-06 16:50:29 +0200

    Adding runit.sh

---
+++ preproc/RADIATIVE_INWATER/runit.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#SBATCH --job-name=1D-RTM
+#SBATCH -N1
+#SBATCH --ntasks-per-node=36
+#SBATCH --time=6:30:00 
+##SBATCH --mem=100gb    
+#SBATCH --account=OGS_dev_1
+#SBATCH --partition=gll_usr_prod
+
+cd $SLURM_SUBMIT_DIR
+
+source env.sh
+
+date
+srun -n $SLURM_NPROCS python launcher.py
+date

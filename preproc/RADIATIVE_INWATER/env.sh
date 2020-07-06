2a4232a2cac06412fff450475b046d04abbf4b8d
Elena <eterzic@inogs.it>
2020-07-06 16:34:45 +0200

    Modify environment

--- preproc/RADIATIVE_INWATER/env.sh
+++ preproc/RADIATIVE_INWATER/env.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+source ~/sequence.sh
+
 module load profile/base
 module load autoload
 module load hdf5/1.8.18--intel--pe-xe-2018--binary netcdf/4.6.1--intel--pe-xe-2018--binary
@@ -11,8 +13,6 @@ module load netcdf/4.6.1--intel--pe-xe-2018--binary
 module load netcdff/4.4.4--intel--pe-xe-2018--binary
 module load cmake/3.12.0
 module load numpy/1.15.2--python--2.7.12
-
-source ~/sequence.sh
 
 export PYTHONPATH=/galileo/home/userexternal/eterzic0/BIT_SEA_mod/bit.sea/:$PYTHONPATH
 

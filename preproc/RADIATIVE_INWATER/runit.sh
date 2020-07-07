106e5ebd8a979e68a19c2fd94c21667883926543
Elena <eterzic@inogs.it>
2020-07-07 15:35:19 +0200

    Shortening the simulation time

--- preproc/RADIATIVE_INWATER/runit.sh
+++ preproc/RADIATIVE_INWATER/runit.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name=1D-RTM
 #SBATCH -N1
 #SBATCH --ntasks-per-node=36
-#SBATCH --time=6:30:00 
+#SBATCH --time=0:30:00 
 ##SBATCH --mem=100gb    
 #SBATCH --account=OGS_dev_1
 #SBATCH --partition=gll_usr_prod

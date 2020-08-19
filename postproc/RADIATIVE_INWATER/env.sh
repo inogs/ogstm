e3e55fef877fa39b7b125ad034216bc237554181
Elena Terzic <eterzic0@r033c01s03.galileo.cineca.it>
2020-08-19 10:17:28 +0200

    Adding the env.sh

---
+++ postproc/RADIATIVE_INWATER/env.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+source ~/sequence.sh
+
+module load profile/base
+module load autoload
+module load hdf5/1.8.18--intel--pe-xe-2018--binary netcdf/4.6.1--intel--pe-xe-2018--binary
+module load mpi4py/3.0.0--intelmpi--2018--binary
+module load profile/advanced
+module load intel/pe-xe-2018--binary
+module load intelmpi/2018--binary
+module load netcdf/4.6.1--intel--pe-xe-2018--binary
+module load netcdff/4.4.4--intel--pe-xe-2018--binary
+module load cmake/3.12.0
+module load numpy/1.15.2--python--2.7.12
+
+export PYTHONPATH=/galileo/home/userexternal/eterzic0/BIT_SEA_mod/bit.sea/:$PYTHONPATH
+
+export ONLINE_REPO=/gpfs/scratch/userexternal/eterzic0/ONLINE/
+export STATIC_REPO=/gpfs/scratch/userexternal/eterzic0/STATIC/
+
+

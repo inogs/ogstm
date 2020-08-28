102edc8a8391758ae6fbf1848e07513ce6d46f42
Elena <eterzic@inogs.it>
2020-08-28 12:26:14 +0200

    Adding a script to submit a lot of plotting jobs - magic!

---
+++ postproc/RADIATIVE_INWATER/run_plots.py
@@ -0,0 +1,27 @@
+#!/bin/env python
+
+from __future__ import print_function, division
+import csv
+import glob
+import numpy as np
+import os, sys
+
+SIM_MAIN_FOLDER = sys.argv[1]                # '/gpfs/scratch/userexternal/eterzic0/1D_RTM/'
+
+for SIM in os.listdir(SIM_MAIN_FOLDER):
+
+	FOLDER   = os.path.join(SIM_MAIN_FOLDER, SIM)
+
+	CSV_FILE = glob.glob(FOLDER + '/*.csv')[0]
+
+	print('Plotting simulations %s ... '%SIM, end = '')
+
+	os.system('sbatch plot_Ed.sh %s %s' %(FOLDER, CSV_FILE))
+	os.system('sbatch plot_Kd.sh %s %s' %(FOLDER, CSV_FILE))
+	os.system('sbatch plot_Kd_CLIM.sh %s %s' %(FOLDER, CSV_FILE))
+	os.system('sbatch plot_Kd_CLIM_WE.sh %s %s' %(FOLDER, CSV_FILE))
+	os.system('sbatch plot_pcolor_Kd.sh %s %s' %(FOLDER, CSV_FILE))
+	os.system('sbatch plot_pcolor_Kd490_SAT.sh %s %s' %(FOLDER, CSV_FILE))
+
+	print('OK')
+

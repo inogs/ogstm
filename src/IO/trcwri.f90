7301f062302d090a198e6f4392966a98e8cfc070
Gianluca Coidessa <gcoidess@r033c01s03.galileo.cineca.it>
2020-10-28 17:04:19 +0100

    parallel rst

--- src/IO/trcwri.f90
+++ src/IO/trcwri.f90
@@ -1,161 +1,133 @@
-      SUBROUTINE trcwri(datestring)
+        SUBROUTINE trcwri(datestring)
         ! gcoidess developing
 
-      USE myalloc
-      USE IO_mem
-      USE calendar
-      USE TIME_MANAGER
-      use mpi
-      USE ogstm_mpi_module
+        USE myalloc
+        USE IO_mem
+        USE calendar
+        USE TIME_MANAGER
+        use mpi
+        USE ogstm_mpi_module
+
+        USE MPI_GATHER_INFO
+
+        USE MATRIX_VARS
+        USE NODES_MODULE
+        USE DTYPE_PROCS_STRING_MODULE
 
 
-      IMPLICIT NONE
-      CHARACTER(LEN=17), INTENT(IN) :: datestring
 
-
+        IMPLICIT NONE
+        CHARACTER(LEN=17), INTENT(IN) :: datestring
 
 !----------------------------------------------------------------------
 ! local declarations
 ! ==================
-      double precision ::  Miss_val =1.e20
-      INTEGER jk,jj,ji,jn
-      double precision julian
+        double precision ::  Miss_val =1.e20
+        INTEGER jk,jj,ji,jn
+        double precision julian
 
 
-      CHARACTER(LEN=37) filename
+        CHARACTER(LEN=37) filename
 
-      CHARACTER(LEN=3) varname
+        CHARACTER(LEN=3) varname
 
-      INTEGER idrank, ierr, istart, jstart, iPe, iPd, jPe, jPd, status(MPI_STATUS_SIZE)
-      INTEGER irange, jrange
-      INTEGER totistart, totiend, relistart, reliend
-      INTEGER totjstart, totjend, reljstart, reljend
-      INTEGER ind1, i_contribution, j_contribution
+        INTEGER idrank, ierr, istart, jstart, iPe, iPd, jPe, jPd, status(MPI_STATUS_SIZE)
+        INTEGER irange, jrange
+        INTEGER totistart, totiend, relistart, reliend
+        INTEGER totjstart, totjend, reljstart, reljend
+        INTEGER ind1, i_contribution, j_contribution
+        CHARACTER(LEN=20)  var_to_store
+        INTEGER :: COUNTER_VAR_TRCWRI, n_dumping_cycles, jv, ivar, writing_rank, ind_col,  
 
+        filename = 'RST.20111231-15:30:00.N1p.nc'
+        julian=datestring2sec(datestring)
+        if(lwp)then
+                     write(*,*)'trcwri ------------  myrank =', myrank ,' datestring = ', datestring
+        end if
 
-       filename = 'RST.20111231-15:30:00.N1p.nc'
+        trcwriparttime = MPI_WTIME() ! cronometer-start
 
-       julian=datestring2sec(datestring)
+        call mppsync()
 
-       if(lwp)write(*,*) 'trcwri ------------  myrank =',myrank,' datestring = ',  datestring
-
-       trcwriparttime = MPI_WTIME() ! cronometer-start
-
-      call mppsync()
-
-      buf     = Miss_val
-      bufftrn = Miss_val
-      if (lwp) tottrn = Miss_val
-
-       DO jn=1,jptra
-        if(myrank == 0) then
-           istart = nimpp
-           jstart = njmpp
-           iPd = nldi
-           iPe = nlei
-           jPd = nldj
-           jPe = nlej
-           irange    = iPe - iPd + 1
-           jrange    = jPe - jPd + 1
-           totistart = istart + iPd - 1
-           totiend   = totistart + irange - 1
-           totjstart = jstart + jPd - 1
-           totjend   = totjstart + jrange - 1
-           relistart = 1 + iPd - 1
-           reliend   = relistart + irange - 1
-           reljstart = 1 + jPd - 1
-           reljend   = reljstart + jrange - 1
+        buf = Miss_val
+        bufftrn = Miss_val
+        if (WRITING_RANK_WR) tottrn = Miss_val
 
 
-            do ji =1 , jpi
-                  do jj =1 , jpj
-                        do jk =1 , jpk
-                              if (tmask(jk,jj,ji).eq.1) then
-                                    buf(jk,jj,ji) = trn(jk,jj,ji, jn)
-                              endif
-                        enddo
-                  enddo
-            enddo
-           tottrn  (:,totjstart:totjend,totistart:totiend)= buf(:, reljstart:reljend,relistart:reliend)
+        n_dumping_cycles = matrix_state_2_row
 
+        COUNTER_VAR_TRCWRI = 1
 
+        RESTARTS_LOOP: DO jv = 1, n_dumping_cycles
+                                
+                DO ivar = 1 , nodes
+        
+                        writing_rank = writing_procs(ivar)
+                        IF (COUNTER_VAR_TRCWRI > JPTRA)then
+                                EXIT
+                        ELSE
 
+                                do ji =1 , jpi
+                                        i_contribution= jpk*jpj * (ji - 1 )
+                                        do jj =1 , jpj
+                                                j_contribution=jpk*(jj-1)
+                                                do jk =1 , jpk
+                                                        ind1 = jk + j_contribution + i_contribution
+                                                        if (tmask(jk,jj,ji).eq.1) then
+                                                                bufftrn(ind1)= trn(jk,jj,ji,COUNTER_VAR_TRCWRI)
+                                                        endif
+                                                enddo
+                                        enddo
+                                enddo
+                                COUNTER_VAR_TRCWRI = COUNTER_VAR_TRCWRI + 1
 
-      do idrank = 1,mpi_glcomm_size-1
-         call MPI_RECV(jpi_rec , 1,                  mpi_integer, idrank,  1,mpi_comm_world, status, ierr)
-         call MPI_RECV(jpj_rec , 1,                  mpi_integer, idrank,  2,mpi_comm_world, status, ierr)
-         call MPI_RECV(istart  , 1,                  mpi_integer, idrank,  3,mpi_comm_world, status, ierr)
-         call MPI_RECV(jstart  , 1,                  mpi_integer, idrank,  4,mpi_comm_world, status, ierr)
-         call MPI_RECV(iPe     , 1,                  mpi_integer, idrank,  5,mpi_comm_world, status, ierr)
-         call MPI_RECV(jPe     , 1,                  mpi_integer, idrank,  6,mpi_comm_world, status, ierr)
-         call MPI_RECV(iPd     , 1,                  mpi_integer, idrank,  7,mpi_comm_world, status, ierr)
-         call MPI_RECV(jPd     , 1,                  mpi_integer, idrank,  8,mpi_comm_world, status, ierr)
-         call MPI_RECV(bufftrn,   jpi_rec*jpj_rec*jpk, mpi_real8, idrank, 11,mpi_comm_world, status, ierr)
+                                CALL MPI_GATHERV(bufftrn, sendcount, MPI_DOUBLE_PRECISION, bufftrn_TOT,jprcv_count, jpdispl_count,MPI_DOUBLE_PRECISION, writing_rank,MPI_COMM_WORLD, IERR)
 
+                        END IF
 
+                END DO
 
-           irange    = iPe - iPd + 1
-           jrange    = jPe - jPd + 1
-           totistart = istart + iPd - 1
-           totiend   = totistart + irange - 1
-           totjstart = jstart + jPd - 1
-           totjend   = totjstart + jrange - 1
-           relistart = 1 + iPd - 1
-           reliend   = relistart + irange - 1
-           reljstart = 1 + jPd - 1
-           reljend   = reljstart + jrange - 1
+                if(WRITING_RANK_WR) then
+                        ind_col = (myrank / n_ranks_per_node)+1
+                        var_to_store =matrix_state_2(jv,ind_col)%var_name
+                        IF (var_to_store == "novars_input")then
+                                EXIT
+                        ELSE
+                                DO idrank = 0,mpi_glcomm_size-1
+                                        ! ******* WRITING RANK sets
+                                        ! indexes of tot matrix where to
+                                        ! place buffers of idrank
+                                        irange    = iPe_a(idrank+1) -iPd_a(idrank+1) + 1
+                                        jrange    = jPe_a(idrank+1) -jPd_a(idrank+1) + 1
+                                        totistart = istart_a(idrank+1) +iPd_a(idrank+1) - 1
+                                        totiend   = totistart + irange -1
+                                        totjstart = jstart_a(idrank+1) +jPd_a(idrank+1) - 1
+                                        totjend   = totjstart + jrange -1
+                                        relistart = 1 + iPd_a(idrank+1)- 1
+                                        reliend   = relistart + irange -1
+                                        reljstart = 1 + jPd_a(idrank+1)- 1
+                                        reljend   = reljstart + jrange -1
+                                        ! **** ASSEMBLING *** WRITING
+                                        ! RANK  puts in tot matrix
+                                        ! buffer received by idrank
+                                        do ji =totistart,totiend
+                                                i_contribution   =jpk*jpj_rec_a(idrank+1)*(ji-1-totistart+ relistart)
+                                                do jj =totjstart,totjend
+                                                        j_contribution =jpk*(jj-1-totjstart+ reljstart)
+                                                        do jk =1, jpk
+                                                                ind1 =jk + j_contribution + i_contribution
+                                                                tottrn(jk,jj,ji)=bufftrn_TOT(ind1+jpdispl_count(idrank+1))
+                                                        enddo
+                                                enddo
+                                        enddo
+                                END DO
 
-          do ji =totistart,totiend
-            i_contribution   = jpk*jpj_rec*(ji-1-totistart+ relistart)
-            do jj =totjstart,totjend
-              j_contribution = jpk*(jj-1-totjstart+ reljstart)
-              do jk =1, jpk
-                  ind1 = jk + j_contribution + i_contribution
-                  tottrn(jk,jj,ji)= bufftrn(ind1)
-              enddo
-            enddo
-          enddo
+                                filename = 'RESTARTS/RST.'//datestring//'.'//var_to_store//'.nc'
 
-          enddo ! do idrank = 1, size-1
-       else !myrank != 0
-           do ji =1 , jpi
-            i_contribution= jpk*jpj * (ji - 1 )
-            do jj =1 , jpj
-             j_contribution=jpk*(jj-1)
-             do jk =1 , jpk
-              ind1 = jk + j_contribution + i_contribution
-              if (tmask(jk,jj,ji).eq.1) then
-                 bufftrn(ind1)= trn(jk,jj,ji, jn)
-              endif
-             enddo
-            enddo
-           enddo
-           call MPI_SEND(jpi      , 1         ,mpi_integer, 0,  1, mpi_comm_world,ierr)
-           call MPI_SEND(jpj      , 1         ,mpi_integer, 0,  2, mpi_comm_world,ierr)
-           call MPI_SEND(nimpp    , 1         ,mpi_integer, 0,  3, mpi_comm_world,ierr)
-           call MPI_SEND(njmpp    , 1         ,mpi_integer, 0,  4, mpi_comm_world,ierr)
-           call MPI_SEND(nlei     , 1         ,mpi_integer, 0,  5, mpi_comm_world,ierr)
-           call MPI_SEND(nlej     , 1         ,mpi_integer, 0,  6, mpi_comm_world,ierr)
-           call MPI_SEND(nldi     , 1         ,mpi_integer, 0,  7, mpi_comm_world,ierr)
-           call MPI_SEND(nldj     , 1         ,mpi_integer, 0,  8, mpi_comm_world,ierr)
-           call MPI_SEND(bufftrn  ,jpk*jpj*jpi,  mpi_real8, 0, 11, mpi_comm_world,ierr)
-       endif ! if myrank = 0
+                                CALL write_restart(filename,var_to_store,julian, deflate_rst, deflate_level_rst)
 
-
-        if(myrank == 0) then
-
-            varname=ctrcnm(jn)
-            filename = 'RESTARTS/RST.'//datestring//'.'//varname//'.nc'
-
-        CALL write_restart(filename,varname,julian, deflate_rst, deflate_level_rst)
-
-        endif ! if myrank = 0
-      END DO ! DO jn=1,jptra
-
-
-       trcwriparttime = MPI_WTIME() - trcwriparttime
-       trcwritottime = trcwritottime + trcwriparttime
-
-
-
-      END SUBROUTINE trcwri
+                        END IF
+                END IF
+        END DO RESTARTS_LOOP
+                        
+        END SUBROUTINE trcwri

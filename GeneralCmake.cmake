c685fed3fb35c22f123d607c7727731ca1db0702
Stefano Campanella <15182642+stefanocampanella@users.noreply.github.com>
2022-10-06 12:16:46 +0200

    minimize diffs within GeneralCmake.cmake

--- GeneralCmake.cmake
+++ GeneralCmake.cmake
@@ -2,26 +2,34 @@
 # author : E.Pascolo, S.Bna, L.Calori
 
 # CMAKE setting
-cmake_minimum_required(VERSION 3.20)
-project(OGSTM)
-set(CMAKE_MODULE_PATH ${CMAKE_SOURCE_DIR}/cmake/)
+cmake_minimum_required (VERSION 3.20)
+project (OGSTM Fortran C)
+set (CMAKE_MODULE_PATH ${CMAKE_SOURCE_DIR}/cmake/)
 enable_language(Fortran C)
-
+set (NETCDF_F90 "YES")
 find_package(MPI REQUIRED)
-include_directories(${MPI_Fortran_INCLUDE_PATH})
-link_directories(${MPI_Fortran_LIBRARIES})
-
 find_package(NetCDF REQUIRED)
-set(NETCDF_F90 "YES")
-include_directories(${NETCDF_INCLUDES_C})
-include_directories(${NETCDFF_INCLUDES_F90})
-
 find_package(BFM REQUIRED)
-include_directories(${BFM_INCLUDES})
 
-if(NOT CMAKE_BUILD_TYPE)
-    set(CMAKE_BUILD_TYPE RELEASE CACHE STRING "Choose the type of build, options are: Debug Release." FORCE)
-endif()
+if (NOT CMAKE_BUILD_TYPE)
+  set (CMAKE_BUILD_TYPE RELEASE CACHE STRING
+      "Choose the type of build, options are: Debug Release."
+      FORCE)
+endif (NOT CMAKE_BUILD_TYPE)
+
+get_filename_component (default_prefix "./bin" ABSOLUTE)
+set (CMAKE_INSTALL_PREFIX ${default_prefix} CACHE STRING
+      "Choose the installation directory; by default it installs in the OGSTM directory."
+      FORCE)
+
+# COMPILER
+
+add_definitions(-Dkey_trahdfcoef1d -Dkey_trahdfbilap -Dkey_trc_smolar)
+add_definitions(-Dkey_trc_hdfbilap -Dkey_trc_dmp -Dkey_kef -Dkey_trc_sed )
+add_definitions(-Dkey_mpp -Dkey_mpp_mpi)
+IF (BFMv2)
+    add_definitions(-DBFMv2)
+ENDIF()
 
 if(CMAKE_Fortran_COMPILER_ID MATCHES "Intel")
     set(CMAKE_Fortran_FLAGS_RELEASE " -fno-math-errno -O2 -xAVX -qopt-report5 -g -cpp -align array64byte") #-qopenmp
@@ -40,22 +48,21 @@ else()
     set(CMAKE_Fortran_FLAGS_DEBUG   "-O0 -g")
 endif()
 
-get_filename_component (default_prefix "./bin" ABSOLUTE)
-set(CMAKE_INSTALL_PREFIX ${default_prefix} CACHE STRING "Choose the installation directory; by default it installs in the OGSTM directory." FORCE)
+#include
+include_directories(${MPI_Fortran_INCLUDE_PATH})
+link_directories(${MPI_Fortran_LIBRARIES})
+include_directories(${BFM_INCLUDES})
+include_directories(${NETCDF_INCLUDES_C})
+include_directories(${NETCDFF_INCLUDES_F90})
 
-add_definitions(-Dkey_trahdfcoef1d -Dkey_trahdfbilap -Dkey_trc_smolar)
-add_definitions(-Dkey_trc_hdfbilap -Dkey_trc_dmp -Dkey_kef -Dkey_trc_sed )
-add_definitions(-Dkey_mpp -Dkey_mpp_mpi)
-if (BFMv2)
-    add_definitions(-DBFMv2)
-endif()
-
-set(FOLDERS BIO  General  IO  MPI  namelists  PHYS BC)
-foreach(FOLDER ${FOLDERS})
-    file(GLOB TMP src/${FOLDER}/*)
-    list (APPEND FORTRAN_SOURCES ${TMP})
+# Search Fortran module to compile
+set( FOLDERS BIO  General  IO  MPI  namelists  PHYS BC)
+  foreach(FOLDER ${FOLDERS})
+  file(GLOB TMP src/${FOLDER}/*)
+  list (APPEND FORTRAN_SOURCES ${TMP})
 endforeach()
 
-add_library(ogstm_lib ${FORTRAN_SOURCES})
-add_executable(ogstm.xx application/ogstm_main_caller.f90)
+#building
+add_library( ogstm_lib ${FORTRAN_SOURCES})
+add_executable (ogstm.xx application/ogstm_main_caller.f90)
 target_link_libraries(ogstm.xx ogstm_lib ${NETCDFF_LIBRARIES_F90} ${BFM_LIBRARIES} MPI::MPI_Fortran)

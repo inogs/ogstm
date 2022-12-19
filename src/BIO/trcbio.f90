7a12240da18ba854112763724f00935286fc4167
Stefano Campanella <15182642+stefanocampanella@users.noreply.github.com>
2022-12-19 09:32:24 +0100

    Fix diagnostics according to the new BFM block interface

--- src/BIO/trcbio.f90
+++ src/BIO/trcbio.f90
@@ -50,10 +50,10 @@ SUBROUTINE trcbio
 
   double precision, dimension(jpi * jpj * jpk, 4) :: sediPPY
   double precision, dimension(jpi * jpj * jpk, jptra_dia) :: local_D3DIAGNOS
-  double precision, dimension(jptra_dia_2d) :: local_D2DIAGNOS
+  double precision, dimension(jpi * jpj, jptra_dia_2d) :: local_D2DIAGNOS
   double precision, dimension(jpi * jpj * jpk, 11) :: er
 
-  integer :: jk, jj, ji, jn, jlinear, bottom
+  integer :: jk, jj, ji, jn, jlinear2d, jlinear3d, bottom
   double precision :: correct_fact, gdept_local, gdeptmax_local
 
   BIOparttime = MPI_WTIME()
@@ -77,8 +77,8 @@ SUBROUTINE trcbio
         else
           bottom = mbathy(jj, ji)
           do jk = 1, bottom
-            jlinear = jk + (jj - 1) * jpk + (ji - 1) * jpk * jpj
-            D3STATE(jlinear, jn) = trn(jk, jj, ji, jn)
+            jlinear3d = jk + (jj - 1) * jpk + (ji - 1) * jpk * jpj
+            D3STATE(jlinear3d, jn) = trn(jk, jj, ji, jn)
           end do
         end if
       end do
@@ -93,19 +93,19 @@ SUBROUTINE trcbio
       else
         bottom = mbathy(jj, ji)
         do jk = 1, bottom
-          jlinear = jk + (jj - 1) * jpk + (ji - 1) * jpk * jpj
+          jlinear3d = jk + (jj - 1) * jpk + (ji - 1) * jpk * jpj
           if (jk .eq. 1) then
-            er(jlinear, 4) = ice ! from 0 to 1 adimensional
-            er(jlinear, 5) = ogstm_co2(jj, ji) ! CO2 Mixing Ratios (ppm)  390
-            er(jlinear, 7) = DAY_LENGTH(jj, ji) ! fotoperiod expressed in hours
-            er(jlinear, 9) = vatm(jj, ji) ! wind speed (m/s)
+            er(jlinear3d, 4) = ice ! from 0 to 1 adimensional
+            er(jlinear3d, 5) = ogstm_co2(jj, ji) ! CO2 Mixing Ratios (ppm)  390
+            er(jlinear3d, 7) = DAY_LENGTH(jj, ji) ! fotoperiod expressed in hours
+            er(jlinear3d, 9) = vatm(jj, ji) ! wind speed (m/s)
           end if
-          er(jlinear, 1) = tn(jk, jj, ji) ! Temperature (Celsius)
-          er(jlinear, 2) = sn(jk, jj, ji) ! Salinity PSU
-          er(jlinear, 3) = rho(jk, jj, ji) ! Density Kg/m3
-          er(jlinear, 6) = instant_par(COMMON_DATEstring, xpar(jk, jj, ji)) ! PAR umoles/m2/s | Watt to umoles photons W2E=1./0.217
-          er(jlinear, 8) = e3t(jk, jj, ji) ! depth in meters of the given cell
-          er(jlinear, 10) = ogstm_PH(jk, jj, ji) ! 8.1
+          er(jlinear3d, 1) = tn(jk, jj, ji) ! Temperature (Celsius)
+          er(jlinear3d, 2) = sn(jk, jj, ji) ! Salinity PSU
+          er(jlinear3d, 3) = rho(jk, jj, ji) ! Density Kg/m3
+          er(jlinear3d, 6) = instant_par(COMMON_DATEstring, xpar(jk, jj, ji)) ! PAR umoles/m2/s | Watt to umoles photons W2E=1./0.217
+          er(jlinear3d, 8) = e3t(jk, jj, ji) ! depth in meters of the given cell
+          er(jlinear3d, 10) = ogstm_PH(jk, jj, ji) ! 8.1
 #ifdef gdept1d
           gdept_local = gdept(jk)
           gdeptmax_local = gdept(jpk)
@@ -120,24 +120,19 @@ SUBROUTINE trcbio
           else
             correct_fact = 0.0D0
           end if
-          er(jlinear, 11) = correct_fact * (gdeptmax_local - gdept_local) / gdept_local
+          er(jlinear3d, 11) = correct_fact * (gdeptmax_local - gdept_local) / gdept_local
         end do
       end if
     end do
   end do
    
-  write (*, *) "TRA surf: ", tra(1, :, :, 49)
-  write (*, *) "TRA min, argmin: ", minval(tra(:, :, :, 49)), minloc(tra(:, :, :, 49))
-  write (*, *) "d3source min, argmin: ", minval(d3source(:, 49)), minloc(d3source(:, 49))
-  write (*, *) "d3state min, argmin: ", minval(d3state(:, 49)), minloc(d3state(:, 49))
   call BFM1D_Input_EcologyDynamics(mbathy, er) ! here mbathy was bottom
   call BFM1D_reset()
   call EcologyDynamics()
   call BFM1D_Output_EcologyDynamics(sediPPY, local_D3DIAGNOS, local_D2DIAGNOS)
-  write (*, *) "d3source min, argmin: ", minval(d3source(:, 49)), minloc(d3source(:, 49))
-  write (*, *) "d3state min, argmin: ", minval(d3state(:, 49)), minloc(d3state(:, 49))
 
-  do jn = 1, jptra ! max(4, jptra, jptra_dia)
+  ! The following copies could be avoided
+  do jn = 1, max(4, jptra, jptra_dia)
     do ji = 1, jpi
       do jj = 1, jpj
         if (bfmmask(1, jj, ji) == 0) then
@@ -145,38 +140,37 @@ SUBROUTINE trcbio
         else
           bottom = mbathy(jj, ji)
           do jk = 1, bottom
-            jlinear = jk + (jj - 1) * jpk + (ji - 1) * jpk * jpj
-            !if (jn .le. jptra) then
-              tra(jk, jj, ji, jn) = tra(jk, jj, ji, jn) + D3SOURCE(jlinear, jn) ! trend
-            !end if
-            !if (jn .le. jptra_dia) then 
-            !  tra_DIA(jk, jj, ji, jn) = local_D3DIAGNOS(jlinear, jn)
-            !end if
-            !if (jn .le. 4) then
-            !  ogstm_sediPI(jk, jj, ji, jn) = sediPPY(jlinear, jn)   ! BFM output of sedimentation speed (m/d)
-            !end if
+            jlinear3d = jk + (jj - 1) * jpk + (ji - 1) * jpk * jpj
+            if (jn .le. jptra) then
+             tra(jk, jj, ji, jn) = tra(jk, jj, ji, jn) + D3SOURCE(jlinear3d, jn) ! trend
+            end if
+            if (jn .le. jptra_dia) then 
+              tra_DIA(jk, jj, ji, jn) = local_D3DIAGNOS(jlinear3d, jn)
+            end if
+            if (jn .le. 4) then
+              ogstm_sediPI(jk, jj, ji, jn) = sediPPY(jlinear3d, jn)   ! BFM output of sedimentation speed (m/d)
+            end if
           end do
         end if
       end do
     end do
   end do
   
-  write (*, *) "TRA min, argmin: ", minval(tra(:, :, :, 49)), minloc(tra(:, :, :, 49))
-
-  !do ji = 1, jpi
-  !  do jj = 1, jpj
-  !    if (bfmmask(1, jj, ji) == 0) then
-  !      cycle
-  !    else
-  !      bottom = mbathy(jj, ji)
-  !      tra_DIA_2d(:, jj, ji) = local_D2DIAGNOS(:)
-  !      do jk = 1, bottom
-  !        jlinear = jk + (jj - 1) * jpk + (ji - 1) * jpk * jpj
-  !        ogstm_PH(jk, jj, ji) = local_D3DIAGNOS(jlinear, pppH) ! Follows solver guess, put 8.0 if pppH is not defined
-  !      end do
-  !    end if
-  !  end do
-  !end do
+  do ji = 1, jpi
+    do jj = 1, jpj
+      if (bfmmask(1, jj, ji) == 0) then
+        cycle
+      else
+        bottom = mbathy(jj, ji)
+        jlinear2d = jj + (ji - 1) * jpj
+        tra_DIA_2d(:, jj, ji) = local_D2DIAGNOS(jlinear2d, :)
+        do jk = 1, bottom
+          jlinear3d = jk + (jj - 1) * jpk + (ji - 1) * jpk * jpj
+          ogstm_PH(jk, jj, ji) = local_D3DIAGNOS(jlinear3d, pppH) ! Follows solver guess, put 8.0 if pppH is not defined
+        end do
+      end if
+    end do
+  end do
 
   !---------------------------------------------------------------------
   ! BEGIN BC_REFACTORING SECTION

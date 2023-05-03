af6dedb780834fc9f1a97493238d5db016570761
Stefano Campanella <15182642+stefanocampanella@users.noreply.github.com>
2023-05-03 12:01:52 +0200

    improved comparison script

--- testcase/runtests.py
+++ testcase/runtests.py
@@ -1,16 +1,25 @@
+#! /usr/bin/env python
+
 import argparse
 import os
 from pathlib import Path
+from tqdm import tqdm
+from colorama import Fore as fore
+from colorama import Style as stl
+from colorama import init
 import netCDF4 as nc
 import numpy as np
+
+init()
+
 
 parser = argparse.ArgumentParser(prog=__file__, formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument('dir1', type=Path, help="Path of first dir")
 parser.add_argument('dir2', type=Path, help="Path of second dir")
+parser.add_argument('subdir', type=Path, help="Path of common subdir")
 parser.add_argument('--rtol', help="Tolerance used in comparison", default=1e-5, type=float)
 parser.add_argument('--atol', help="Tolerance used in comparison", default=1e-8, type=float)
-parser.add_argument('--log', help="Log level", default='warning')
-parser.add_argument('--progress', help="Show progress bar", default=False, action='store_true')
+parser.add_argument('--noprogress', help="Disable progress bar", default=False, action='store_true')
 
 cli_args = parser.parse_args()
 
@@ -24,29 +33,39 @@ def intersect(A, B):
 def allclose(xs, ys):
     return np.allclose(xs, ys, rtol=cli_args.rtol, atol=cli_args.atol)
 
+def colorize(string, passed=True):
+    return stl.BRIGHT + (fore.GREEN if passed else fore.RED) + string + stl.RESET_ALL
+
+
 def compare_dumps(subdirectory):
     files_1, files_2 = [getfiles(directory / subdirectory) for directory in [cli_args.dir1, cli_args.dir2]]
     common_files = intersect(files_1.keys(), files_2.keys())
     tests = {}
-    for filename in common_files:
+    for filename in tqdm(common_files, disable=cli_args.noprogress):
         ds_1, ds_2 = [nc.Dataset(files[filename], 'r') for files in [files_1, files_2]]
         variables = filter(lambda v: v not in {'lat', 'lon', 'depth'}, intersect(ds_1.variables, ds_2.variables))
         for varname in variables:
             var_1, var_2 = [ds[varname][:] for ds in [ds_1, ds_2]]
             ispassed = allclose(var_1, var_2)
-            tests[varname] = ispassed
             err = np.abs(var_1 - var_2).max()
-            print(f"{varname} ({err=:.2e}):\t{'PASS' if ispassed else 'FAIL'}")
-
-    print(32 * "=")
-    if all(tests.values()):
-        print(f"All tests passed for {subdirectory}")
-    else:
-        print(f"Some tests failed for {subdirectory}")
-    print(32 * "=")
+            tests[varname] = ispassed, err
+    
+    return tests
 
 if __name__ == '__main__':
-    for subdir in ['AVE_FREQ_1', 'AVE_FREQ_2']:
-        compare_dumps(subdir)
-
+    tqdm.write("Running tests\n")
+    tests = compare_dumps(cli_args.subdir)
+   
+    print('\n' + 'VARIABLE' + 8 * ' ' + 'MAX ERROR' + 8 * ' ' + 'RESULT' + 26 * ' ')
+    print(48 * "=")
+    for (varname, (ispassed, err)) in tests.items():
+        tqdm.write(f"{varname:16}{err:.2e}" + 9 * ' ', end='')
+        if ispassed:
+            print(colorize('PASS'))
+        else:
+            print(colorize('FAIL', passed=False))
+    if all(ispassed for ispassed, _ in tests.values()):
+        print(colorize(f"\nAll tests passed for {cli_args.subdir}"))
+    else:
+        print(colorize(f"\nSome tests failed for {cli_args.subdir}", passed=False))
 

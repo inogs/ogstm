7864fa822544ad40380ea6c66ed76d3e0a000954
Loris Lucido <loris.lucido@eviden.com>
2024-05-13 16:58:48 +0200

    trcbio GPU port

--- src/BIO/trcsms.f90
+++ src/BIO/trcsms.f90
@@ -23,6 +23,9 @@
        USE myalloc
        USE mpi
        use simple_timer
+
+       ! XXX: to remove
+       use BIO_mem, only: ogstm_sediPI,ogstm_PH,ogstm_co2
        IMPLICIT NONE
 
 
@@ -36,7 +39,9 @@
        CALL trcopt ! tracers: optical model
        
        call tstart("trcbio")
+       !$acc update device(mbathy,bfmmask,trn,DAY_LENGTH,vatm,tn,sn,rho,xpar,e3t,gdept,ogstm_PH,ogstm_co2)
        CALL trcbio ! tracers: biological model
+       !$acc update host(tra,tra_DIA,tra_DIA_2d,ogstm_sediPI,ogstm_PH)
        call tstop("trcbio")
 
 !! trcsed no updated for time step advancing

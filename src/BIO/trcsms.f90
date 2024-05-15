c0527c64a5e77e9eaa3c29850856a0b8c7fae945
Loris Lucido <loris.lucido@eviden.com>
2024-05-15 15:43:59 +0200

    trcsed GPU port

--- src/BIO/trcsms.f90
+++ src/BIO/trcsms.f90
@@ -24,9 +24,6 @@
        USE mpi
        use simple_timer
 
-       ! XXX: to remove
-       use BIO_mem, only: ogstm_sediPI,ogstm_PH,ogstm_co2
-       USE OPT_mem, only: kef
        IMPLICIT NONE
 
 
@@ -34,20 +31,16 @@
 
        trcsmsparttime = MPI_WTIME() ! cronometer-start
 
-
 !! this first routines are parallelized on vertical slab
 
        call tstart("trcopt")
 
-       !$acc update device(kef,qsr)
        CALL trcopt ! tracers: optical model
 
        call tstop("trcopt")
 
        call tstart("trcbio")
-       !$acc update device(mbathy,bfmmask,trn,DAY_LENGTH,vatm,tn,sn,rho,e3t,gdept,ogstm_PH,ogstm_co2)
        CALL trcbio ! tracers: biological model
-       !$acc update host(tra,tra_DIA,tra_DIA_2d,ogstm_sediPI,ogstm_PH)
        call tstop("trcbio")
 
 !! trcsed no updated for time step advancing

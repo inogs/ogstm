5be009aa71c8c81cfb789f40f9d68ea22831398a
Stefano Campanella <15182642+stefanocampanella@users.noreply.github.com>
2024-07-16 16:43:05 +0200

    Checkout GeneralCmake.cmake and compilers/machine_modules/leonardo.intel from dev_gpu

--- GeneralCmake.cmake
+++ GeneralCmake.cmake
@@ -2,11 +2,12 @@
 # author : E.Pascolo, S.Bna, L.Calori
 
 # CMAKE setting
-cmake_minimum_required (VERSION 3.18)
-project (OGSTM)
+cmake_minimum_required (VERSION 3.20)
+project (OGSTM Fortran C)
 set (CMAKE_MODULE_PATH ${CMAKE_SOURCE_DIR}/cmake/)
 enable_language(Fortran C)
 set (NETCDF_F90 "YES")
+set(CMAKE_NO_SYSTEM_FROM_IMPORTED TRUE)
 find_package(MPI REQUIRED)
 find_package(NetCDF REQUIRED)
 find_package(BFM REQUIRED)
@@ -24,8 +25,6 @@ set (CMAKE_INSTALL_PREFIX ${default_prefix} CACHE STRING
 
 # COMPILER
 
-get_filename_component (Fortran_COMPILER_NAME ${CMAKE_Fortran_COMPILER} NAME)
-set(CMAKE_Fortran_COMPILER ${MPI_Fortran_COMPILER})
 add_definitions(-Dkey_trahdfcoef1d -Dkey_trahdfbilap -Dkey_trc_smolar)
 add_definitions(-Dkey_trc_hdfbilap -Dkey_trc_dmp -Dkey_kef -Dkey_trc_sed )
 add_definitions(-Dkey_mpp -Dkey_mpp_mpi)
@@ -33,23 +32,27 @@ IF (BFMv2)
     add_definitions(-DBFMv2)
 ENDIF()
 
-if (MPI_Fortran_COMPILER MATCHES "mpiifort.*")
-  # mpiifort
-  set (CMAKE_Fortran_FLAGS_RELEASE " -fno-math-errno -O2 -xAVX -qopt-report5 -g -cpp -align array64byte") #-qopenmp
-  set (CMAKE_Fortran_FLAGS_DEBUG   " -O0 -g -cpp -CB -fp-stack-check -check all -traceback -gen-interfaces -warn interfaces -fpe0 -extend-source")
-elseif (MPI_Fortran_COMPILER MATCHES "mpif90.*")
-  # mpif90
-  set (CMAKE_Fortran_FLAGS_RELEASE " -O2  -fimplicit-none -cpp -ffree-line-length-0 ")
-  set (CMAKE_Fortran_FLAGS_DEBUG   " -O0 -g -Wall -Wextra -cpp -fbounds-check -fimplicit-none -ffpe-trap=invalid,overflow -pedantic")
-else ()
-  message ("CMAKE_Fortran_COMPILER full path: " ${CMAKE_Fortran_COMPILER})
-  message ("Fortran compiler: " ${Fortran_COMPILER_NAME})
-  message ("No optimized Fortran compiler flags are known, we just try -O2...")
-  set (CMAKE_Fortran_FLAGS_RELEASE "-O2")
-  set (CMAKE_Fortran_FLAGS_DEBUG   "-O0 -g")
-endif ()
+if(CMAKE_Fortran_COMPILER_ID MATCHES "Intel")
+    set(CMAKE_Fortran_FLAGS_RELEASE " -fno-math-errno -O2 -xCORE-AVX2 -qopt-report5 -g -traceback -cpp -align array64byte") #-qopenmp
+    set(CMAKE_Fortran_FLAGS_DEBUG   " -O0 -g -cpp -CB -fp-stack-check -check all -traceback -gen-interfaces -warn interfaces -fpe0 -extend-source")
+elseif(CMAKE_Fortran_COMPILER_ID MATCHES "GNU")
+    set(CMAKE_Fortran_FLAGS_RELEASE " -O3 -march=native -fimplicit-none -cpp -ffree-line-length-none ")
+    set(CMAKE_Fortran_FLAGS_DEBUG   " -Og -ggdb3 -fimplicit-none -cpp -ffree-line-length-none -Wall -Wextra -fno-omit-frame-pointer -fbounds-check -pedantic -ffpe-trap=invalid,zero,overflow")
+    set(CMAKE_LINKER_FLAGS_DEBUG "${CMAKE_LINKER_FLAGS_DEBUG} -fno-omit-frame-pointer")
+elseif(CMAKE_Fortran_COMPILER_ID MATCHES "NVHPC|PGI")
+    set(CMAKE_Fortran_FLAGS_RELEASE " -Kieee -g -traceback -fast -acc -gpu=pinned -Mextend -Mpreprocess")
+    set(CMAKE_Fortran_FLAGS_DEBUG " -Kieee -g -traceback -O0 -acc -gpu=pinned,debug -Mbounds -Mextend -Mpreprocess -Minfo=accel")
+else()
+    message ("CMAKE_Fortran_COMPILER full path: " ${CMAKE_Fortran_COMPILER})
+    message ("Fortran compiler: " ${CMAKE_Fortran_COMPILER_ID})
+    message ("No optimized Fortran compiler flags are known, we just try -O2...")
+    set(CMAKE_Fortran_FLAGS_RELEASE "-O2")
+    set(CMAKE_Fortran_FLAGS_DEBUG   "-O0 -g")
+endif()
 
 #include
+include_directories(${MPI_Fortran_INCLUDE_PATH})
+link_directories(${MPI_Fortran_LIBRARIES})
 include_directories(${BFM_INCLUDES})
 include_directories(${NETCDF_INCLUDES_C})
 include_directories(${NETCDFF_INCLUDES_F90})
@@ -58,10 +61,11 @@ include_directories(${NETCDFF_INCLUDES_F90})
 set( FOLDERS BIO  General  IO  MPI  namelists  PHYS BC)
   foreach(FOLDER ${FOLDERS})
   file(GLOB TMP src/${FOLDER}/*)
-  list (APPEND FORTRAN_SOURCES ${TMP})
+  list(APPEND FORTRAN_SOURCES ${TMP})
+  list(FILTER FORTRAN_SOURCES EXCLUDE REGEX "test_[a-zA-Z0-9_]*\.[f03]")
 endforeach()
 
 #building
 add_library( ogstm_lib ${FORTRAN_SOURCES})
 add_executable (ogstm.xx application/ogstm_main_caller.f90)
-target_link_libraries( ogstm.xx ogstm_lib ${NETCDFF_LIBRARIES_F90} ${BFM_LIBRARIES})
+target_link_libraries(ogstm.xx ogstm_lib ${NETCDFF_LIBRARIES_F90} ${BFM_LIBRARIES} MPI::MPI_Fortran)

367c6007b5bef569854a247416b9a7273d049674
Loris Lucido <loris.lucido@eviden.com>
2024-12-18 12:54:01 +0100

    prevent double allocation when dimen_jvsed=0

--- src/BIO/SED_mem.f90
+++ src/BIO/SED_mem.f90
@@ -90,6 +90,11 @@
 
       subroutine myalloc_SED_ztra_zwork()
 
+        if(dimen_jvsed .eq. 0) then
+           ! some ranks have nothing to do, don't allocate twice
+           if (allocated(ztra)) return
+        endif
+
         allocate(ztra(nsed,dimen_jvsed))
         allocate(zwork(jpk,nsed,dimen_jvsed))
         !$acc enter data create(ztra,zwork)

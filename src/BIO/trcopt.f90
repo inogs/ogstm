4708ad74020ce14880d417e220e91be045b4b100
Loris Lucido <loris.lucido@eviden.com>
2025-01-16 15:52:36 +0100

    trcopt: improve GPU port with more parallelism

--- src/BIO/trcopt.f90
+++ src/BIO/trcopt.f90
@@ -32,39 +32,48 @@
 !!!$omp parallel default(none) private(mytid,jk,ji)
 !!!$omp&                       shared(jj,jpk,jpj,jpi,xpar,conversion,kef,e3t,qsr)
 
-! 1. determination of surface irradiance
+      ! 1. determination of surface irradiance
+
       queue=1
-      ! XXX: this can be improved but no way to test as it doesn't seem to
-      ! impact numerical results
-      !$acc kernels default(present) async(queue)
+      !$acc parallel loop gang vector default(present) collapse(3) async(queue)
       DO ji = 1,jpi
          DO jj = 1,jpj
-
-            ! zpar0m(ji)          = qsr(jj,ji)*conversion
-            ! zpar100(ji)         = zpar0m(ji)*0.01
-            xpar(1,jj,ji) = qsr(jj,ji)*conversion
-            ! zpar(1,ji)          = zpar0m(1)
-            xEPS_ogstm(1,ji)          = kef(jj,ji)
-
-            !! 2. determination of xpar
-            !! ------------------------
-
-            DO jk = 2,jpk
-               xEPS_ogstm(jk,ji) = max(kef(jj,ji),1.D-15) ! avoid denormalized number
-            END DO
-
-            DO jk = 2,jpk
-               xpar(jk,jj,ji) = max( xpar(jk-1,jj,ji) *exp(-1. * xEPS_ogstm(jk-1,ji)* e3t(jk-1,jj,ji)) ,1.D-15) ! avoid denormalized number
-
-            END DO
-
             DO jk = 1,jpk
-               xpar(jk,jj,ji) = max( xpar(jk,jj,ji) * exp(- xEPS_ogstm(jk,ji)* 0.5D+00* e3t(jk,jj,ji) ) ,1.D-15)
+               if (jk .eq. 1) then
+                  xEPS_ogstm(jk,jj,ji)          = kef(jj,ji)
+               else
+                  xEPS_ogstm(jk,jj,ji) = max(kef(jj,ji),1.D-15) ! avoid denormalized number
+               endif
             END DO
+         END DO
+      END DO
 
+      !! 2. determination of xpar
+      !! ------------------------
+
+      !$acc parallel loop gang vector default(present) collapse(2) async(queue)
+      ! NOTE: only collapse the first two loops as the third isn't parallel
+      ! because we read xpar(jk-1,...) and write on xpar(jk,...)
+      DO ji = 1,jpi
+         DO jj = 1,jpj
+            DO jk = 1,jpk
+               if (jk .eq. 1) then
+                  xpar(jk,jj,ji) = qsr(jj,ji)*conversion
+               else
+                  xpar(jk,jj,ji) = max( xpar(jk-1,jj,ji) *exp(-1. * xEPS_ogstm(jk-1,jj,ji)* e3t(jk-1,jj,ji)) ,1.D-15) ! avoid denormalized number
+               endif
+            END DO
+         END DO
+      END DO
+
+      !$acc parallel loop gang vector default(present) collapse(3) async(queue)
+      DO ji = 1,jpi
+         DO jj = 1,jpj
+            DO jk = 1,jpk
+               xpar(jk,jj,ji) = max( xpar(jk,jj,ji) * exp(- xEPS_ogstm(jk,jj,ji)* 0.5D+00* e3t(jk,jj,ji) ) ,1.D-15)
+            END DO
          END DO
       ENDDO
-      !$acc end kernels
       !$acc wait(queue)
 
        trcoptparttime = MPI_WTIME() - trcoptparttime ! cronometer-stop

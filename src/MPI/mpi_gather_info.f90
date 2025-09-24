52e2eb802c33c817774a1b89a0cbedff7e868229
Giorgio Bolzon <gbolzon@ogs.it>
2025-09-24 10:47:43 +0200

    RANKS_PER_NODE has now 4 digits, useful for leonardo (112 cores per node)

--- src/MPI/mpi_gather_info.f90
+++ src/MPI/mpi_gather_info.f90
@@ -70,7 +70,7 @@ MODULE MPI_GATHER_INFO
         double precision, allocatable ::  tottrnIO(:,:,:)
 
 
-        CHARACTER(len=2) :: n_ranks_per_node_char
+        CHARACTER(len=4) :: n_ranks_per_node_char
         !INTEGER :: n_ranks_per_node
 
         CONTAINS

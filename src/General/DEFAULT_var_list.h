f41ed18df7f1150d17126f445fdf87d12832eea0
Paolo Lazzari <plazzari@login01.leonardo.local>
2026-01-16 17:51:31 +0100

    Make ogstm agnostic with respect to biogeochemical model used, remove reference to BFM if key_trc_bfm not active, default model is one passive tracer

---
+++ src/General/DEFAULT_var_list.h
@@ -0,0 +1,26 @@
+      INTEGER, parameter :: jptra=1
+
+      INTEGER, parameter :: jptra_var=1
+
+      INTEGER, parameter :: jptra_flux=1
+
+      INTEGER, parameter :: jptra_dia=jptra_var + jptra_flux
+       
+      INTEGER, parameter :: jptra_dia_2d=1
+
+ 
+! State variables indexes
+        integer,parameter :: ppDEFAULT1=1
+
+
+! Diagnostic indexes
+        integer,parameter :: ppDEFAULT2=2
+
+
+! Flux indexes
+        integer, parameter:: ppDEFAULT3=3
+
+
+! Variables 2d
+        integer, parameter:: ppDEFAULT4=4
+

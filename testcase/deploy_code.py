dea3972811643b224dfd25dff157e345432ed5b5
Paolo Lazzari <plazzari@login07.leonardo.local>
2026-01-20 11:43:33 +0100

    add configuration column for BGC-TYPE

--- testcase/deploy_code.py
+++ testcase/deploy_code.py
@@ -17,9 +17,6 @@ def deploy_code(test):
     CODEPATH = test['Code'].decode() 
     CODEPATH = CODEPATH.replace("~",os.getenv("HOME"))
     os.system("ln -fs  " + CODEPATH +  "OGSTM_BUILD_DBG/ogstm.xx "+ test['Dir'].decode() + "/" )
-
-    namelists= CODEPATH +  "/ogstm/ready_for_model_namelists/* "
-    os.system("cp -pf " + namelists + test['Dir'].decode() + "/")
     
     os.system("cp subgen.py " + test['Dir'].decode() )
     os.system("cp boundaries.nml " + test['Dir'].decode() )
@@ -32,5 +29,21 @@ def deploy_code(test):
     print("   ingv_lon_shift = 0 ")
     print("   is_free_surface = .false.")
 
-    fabm_yaml=  CODEPATH + "/fabm/extern/ogs/fabm_multispectral_2xDetritus.yaml "
-    os.system("cp -pf " + fabm_yaml  + test['Dir'].decode() + "/fabm.yaml")
+    if test['BGC_TYPE'].decode() in ['DEFAULT','Default','default']:
+        os.system("cp -pf namelist.init.default  " +  test['Dir'].decode() + "/namelist.init")
+        os.system("cp -pf namelist.passivetrc.default  " +  test['Dir'].decode() + "/namelist.passivetrc")
+
+    elif test['BGC_TYPE'].decode() in ['BFM','bfm']:
+        namelists= CODEPATH +  "/ogstm/ready_for_model_namelists/* "
+        os.system("cp -pf " + namelists + test['Dir'].decode() + "/")
+
+    elif test['BGC_TYPE'].decode() in ['FABM','fabm']:
+        os.system("cp -pf namelist.init.fabm  " +  test['Dir'].decode() + "/namelist.init")
+        fabm_yaml=  CODEPATH + "/fabm/extern/ogs/fabm_multispectral_2xDetritus.yaml "
+        os.system("cp -pf " + fabm_yaml  + test['Dir'].decode() + "/fabm.yaml")
+
+    else:
+
+        print("BGC_TYPE " + test['BGC_TYPE'].decode() + "wrong/undefined choose within [DEFAULT[Default,default], BFM[bfm], FABM[fabm]]")
+        sys.exit()
+

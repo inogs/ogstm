c4309e3f162e465b79a7570109446068a19f223c
Paolo Lazzari <plazzari@login07.leonardo.local>
2026-01-26 12:37:20 +0100

    allow for greater lenght of restart  filenames and variables specification them.

--- src/IO/trcrst.f90
+++ src/IO/trcrst.f90
@@ -22,7 +22,7 @@
 ! local declarations
 ! ==================
       INTEGER jn, jn_high
-      CHARACTER(LEN=37) filename
+      CHARACTER(LEN=100) filename
       CHARACTER(LEN=100) bkpname
       logical existFile
       logical bkp1hasbeenread,bkp2hasbeenread
@@ -49,7 +49,7 @@
 
          filename = 'RESTARTS/RST.'//DateStart//'.'//trim(ctrcnm(jn))// & 
                 '.nc'
-         CALL readnc_slice_double(filename, 'TRN'//trim(ctrcnm(jn)), trn(:,:,:,jn) )
+         CALL readnc_slice_double(trim(filename), 'TRN'//trim(ctrcnm(jn)), trn(:,:,:,jn) )
 
 
 

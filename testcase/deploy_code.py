e6465d9c6e293c61aee2d50433dbae0bf680c0a0
Marco-Puglia <mpuglia0@login01.leonardo.local>
2026-04-24 09:50:26 +0200

    Added bgc_error.py to house shared error logic and edited the relative files

--- testcase/deploy_code.py
+++ testcase/deploy_code.py
@@ -4,6 +4,8 @@ import numpy as np
 
 from mydtype import *
 
+from bgc_error import raise_bgc_error
+
 import scipy.io.netcdf as NC
 
 import pickle
@@ -73,7 +75,5 @@ def deploy_code(test):
             os.system("python create_ogstm_yaml.py")
             os.system("cp -pf ogstm.yaml " + test['Dir'].decode() + "/ogstm.yaml")
     else:
-
-        print("[ERROR] BGC_TYPE='" + test['BGC_TYPE'].decode() + "' is wrong/undefined. Expected one of: [DEFAULT[Default,default], BFM[bfm], FABM-BFM[fabm-bfm]]")
-        sys.exit()
-
+    
+        raise_bgc_error(test['BGC_TYPE'].decode())

d81ec1fa684af0b53b035e2ad53802b27ded07eb
Paolo Lazzari <plazzari@login05.leonardo.local>
2026-05-18 13:45:48 +0200

    Bug Fix for Parallel I/O when CPU per node > 99, more than two digit required.

--- src/MPI/mpi_gather_info.f90
+++ src/MPI/mpi_gather_info.f90
@@ -70,9 +70,8 @@ MODULE MPI_GATHER_INFO
         double precision, allocatable ::  tottrnIO(:,:,:)
 
 
-        CHARACTER(len=2) :: n_ranks_per_node_char
+        CHARACTER(len=4) :: n_ranks_per_node_char
         INTEGER :: n_ranks_per_node
-        !WRITING_RANK = .FALSE.
 
         CONTAINS
 
